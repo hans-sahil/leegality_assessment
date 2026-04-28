962e92cdfed896bfab2ab2fe392fa5dda1c0edef
Sahil <hanss9545@gmail.com>
2026-04-28 20:21:43 +0530

    showing suspense loaders at the center of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import ProductListingPage from "./ProductListingPage";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
       <ProductListingPage />
     </Suspense>
   );
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,7 +3,13 @@ import ProductDetails from "./ProductDetails";
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
       <ProductDetails params={params} />
     </Suspense>
   );
